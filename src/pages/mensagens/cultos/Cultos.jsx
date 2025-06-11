fcafee6318d4a1d6eba5136e08d17aeea81513c6
Rebeca Amorim <amorimpenhar@gmail.com>
2025-06-11 09:19:23 -0300

    refactor(front)[cultos]: update data structure and standardize property names in cultos component

--- src/pages/mensagens/cultos/Cultos.jsx
+++ src/pages/mensagens/cultos/Cultos.jsx
@@ -6,54 +6,54 @@ import '@styles/actions.css';
 export function Cultos() {
     const cultosData = [
         {
-            titulo: "Culto de Família: Unidos em Cristo",
-            descricao: "Neste culto especial, aprendemos sobre como fortalecer os laços familiares através da fé e do amor cristão.",
-            imagem: "./pages/mensagens/cultos/card-cultos/culto-celebracao.svg",
-            data: "04 de junho de 2025",
-            pregador: "Pastor 1",
-            pastorId: "1",
-            pastorLink: "/pastores#1",
-            slug: "familia-unidos-em-cristo"
+            slug: "familia-unidos-em-cristo",
+            title: "Culto de Família: Unidos em Cristo",
+            desciption: "Neste culto especial, aprendemos sobre como fortalecer os laços familiares através da fé e do amor cristão.",
+            image: "./pages/mensagens/cultos/card-cultos/culto-celebracao.svg",
+            date: "04 de junho de 2025",
+            preacher: "Pastor 1",
+            preacherId: "1",
+            preacherLink: "/pastores#1"
         },
         {
-            titulo: "Culto de Oração: Poder da Intercessão",
-            descricao: "Um momento poderoso de oração onde experimentamos a presença de Deus de forma especial, com muitos testemunhos de cura e libertação.",
-            imagem: "./pages/mensagens/cultos/card-cultos/culto-oracao.svg",
-            data: "31 de maio de 2025",
-            pregador: "Pastor 2",
-            pastorId: "2",
-            pastorLink: "/pastores#2",
-            slug: "oracao-poder-intercessao"
+            slug: "oracao-poder-intercessao",
+            title: "Culto de Oração: Poder da Intercessão",
+            description: "Um momento poderoso de oração onde experimentamos a presença de Deus de forma especial, com muitos testemunhos de cura e libertação.",
+            image: "./pages/mensagens/cultos/card-cultos/culto-oracao.svg",
+            date: "31 de maio de 2025",
+            preacher: "Pastor 2",
+            preacherId: "2",
+            preacherLink: "/pastores#2"
         },
         {
-            titulo: "Culto Jovem: Geração Radical",
-            descricao: "Um culto cheio de energia onde os jovens foram desafiados a viver uma fé autêntica e radical em meio à sociedade atual.",
-            imagem: "./pages/mensagens/cultos/card-cultos/culto-jovem.svg",
-            data: "27 de maio de 2025",
-            pregador: "Pastor 2",
-            pastorId: "2",
-            pastorLink: "/pastores#2",
-            slug: "jovem-geracao-radical"
+            slug: "jovem-geracao-radical",
+            title: "Culto Jovem: Geração Radical",
+            description: "Um culto cheio de energia onde os jovens foram desafiados a viver uma fé autêntica e radical em meio à sociedade atual.",
+            image: "./pages/mensagens/cultos/card-cultos/culto-jovem.svg",
+            date: "27 de maio de 2025",
+            preacher: "Pastor 2",
+            preacherId: "2",
+            preacherLink: "/pastores#2"
         },
         {
-            titulo: "Culto de Celebração: Honra e Glória",
-            descricao: "Um momento especial de adoração e celebração onde experimentamos a glória de Deus por meio de louvores e da pregação da Palavra.",
-            imagem: "./pages/mensagens/cultos/card-cultos/culto-adoracao.svg",
-            data: "25 de maio de 2025",
-            pregador: "Pastor 1",
-            pastorId: "1",
-            pastorLink: "/pastores#1",
-            slug: "celebracao-honra-gloria"
+            slug: "celebracao-honra-gloria",
+            title: "Culto de Celebração: Honra e Glória",
+            description: "Um momento especial de adoração e celebração onde experimentamos a glória de Deus por meio de louvores e da pregação da Palavra.",
+            image: "./pages/mensagens/cultos/card-cultos/culto-adoracao.svg",
+            date: "25 de maio de 2025",
+            preacher: "Pastor 1",
+            preacherId: "1",
+            preacherLink: "/pastores#1"
         },
         {
-            titulo: "Culto de Casais: Aliança Eterna",
-            descricao: "Um culto dedicado a fortalecer os casamentos, com ensinamentos práticos sobre comunicação, perdão e intimidade no relacionamento conjugal.",
-            imagem: "./pages/mensagens/cultos/card-cultos/culto-casais.svg",
-            data: "20 de maio de 2025",
-            pregador: "Pastor 1",
-            pastorId: "1",
-            pastorLink: "/pastores#1",
-            slug: "casais-alianca-eterna"
+            slug: "casais-alianca-eterna",
+            title: "Culto de Casais: Aliança Eterna",
+            description: "Um culto dedicado a fortalecer os casamentos, com ensinamentos práticos sobre comunicação, perdão e intimidade no relacionamento conjugal.",
+            image: "./pages/mensagens/cultos/card-cultos/culto-casais.svg",
+            date: "20 de maio de 2025",
+            preacher: "Pastor 1",
+            preacherId: "1",
+            preacherLink: "/pastores#1"
         }
     ];
 
@@ -65,21 +65,22 @@ export function Cultos() {
                     Acompanhe os nossos cultos anteriores.
                 </p>
             </header>
+            
             <main className="cultos-main">
                 {cultosData.map((culto, index) => (
                     <article key={index} className="culto-card">
                         <div className="culto-image-container">
-                            <img src={culto.imagem} alt={culto.titulo} className="culto-image" />
+                            <img src={culto.image} alt={culto.title} className="culto-image" />
                         </div>
                         <div className="culto-info">
-                            <h2>{culto.titulo}</h2>
-                            <p className='culto-description'>{culto.descricao}</p>
+                            <h2>{culto.title}</h2>
+                            <p className='culto-description'>{culto.description}</p>
                             <div className="culto-details">
-                                <p><strong>Data:</strong> {culto.data}</p>
+                                <p><strong>Data:</strong> {culto.date}</p>
                                 <p>
                                     <strong>Pastor:</strong>{' '}
-                                    <Link to={culto.pastorLink} className="pastor-link">
-                                        {culto.pregador}
+                                    <Link to={culto.preacherLink} className="pastor-link">
+                                        {culto.preacher}
                                     </Link>
                                 </p>
                             </div>
--- src/pages/mensagens/cultos/details/culto-data.js
+++ src/pages/mensagens/cultos/details/culto-data.js
@@ -1,20 +1,20 @@
 export const cultosData = [
     {
         slug: "familia-unidos-em-cristo",
-        titulo: "Culto de Família: Unidos em Cristo",
-        data: "04 de junho de 2025",
-        pregador: "Pastor 1",
-        pastorLink: "/pastores#1",
-        tema: "Construindo Famílias Sólidas na Palavra",
-        imagemFundo: "/pages/mensagens/cultos/card-cultos/culto-celebracao.svg",
-        resumo: "No culto de família 'Unidos em Cristo', fomos convidados a refletir sobre o papel central que Deus deve ocupar em nossas famílias. Aprendemos princípios bíblicos essenciais para fortalecer os laços familiares através da fé, comunicação e perdão mútuo. O culto destacou como podemos enfrentar os desafios contemporâneos mantendo nossa unidade familiar alicerçada em Cristo.",
-        referencias: [
+        title: "Culto de Família: Unidos em Cristo",
+        date: "04 de junho de 2025",
+        preacher: "Pastor 1",
+        preacherLink: "/pastores#1",
+        theme: "Construindo Famílias Sólidas na Palavra",
+        backgroundImage: "/pages/mensagens/cultos/card-cultos/culto-celebracao.svg",
+        abstract: "No culto de família 'Unidos em Cristo', fomos convidados a refletir sobre o papel central que Deus deve ocupar em nossas famílias. Aprendemos princípios bíblicos essenciais para fortalecer os laços familiares através da fé, comunicação e perdão mútuo. O culto destacou como podemos enfrentar os desafios contemporâneos mantendo nossa unidade familiar alicerçada em Cristo.",
+        references: [
             "Salmos 29:1-2 - Tributar ao Senhor honra e glória",
             "João 4:23-24 - Adoradores em espírito e em verdade",
             "Romanos 12:1 - Oferecer o corpo como culto racional",
             "Hebreus 13:15 - Sacrifício de louvor como fruto dos lábios"
         ],
-        pontosPrincipais: [
+        mainPoints: [
             {
                 titulo: "Adoração como estilo de vida",
                 descricao: "Exploramos o conceito de adoração além dos momentos de culto coletivo, entendendo-a como um estilo de vida que perpassa todas as áreas de nossa existência. Discutimos como nossas escolhas diárias, relacionamentos e até mesmo nosso trabalho podem ser expressões de adoração a Deus."
@@ -32,146 +32,146 @@ export const cultosData = [
                 descricao: "Discutimos a importância de fomentar um ambiente de adoração autêntica em nossa igreja, onde todos se sintam convidados a expressar seu louvor independentemente de talentos musicais. Abordamos como podemos usar diferentes expressões artísticas para glorificar a Deus."
             }
         ],
-        conclusao: "A verdadeira adoração nunca deixa as coisas como estão. Quando entramos na presença de Deus com corações sinceros, algo sempre muda - seja em nós, à nossa volta ou em ambos. A adoração é o nosso convite para Deus reinar em cada área da nossa vida, e também nossa declaração de guerra contra tudo o que tenta nos afastar dEle. Quando nos comprometemos com uma vida de adoração autêntica, nos tornamos canais da Sua glória e testemunhas vivas do Seu poder transformador.",
+        conclusion: "A verdadeira adoração nunca deixa as coisas como estão. Quando entramos na presença de Deus com corações sinceros, algo sempre muda - seja em nós, à nossa volta ou em ambos. A adoração é o nosso convite para Deus reinar em cada área da nossa vida, e também nossa declaração de guerra contra tudo o que tenta nos afastar dEle. Quando nos comprometemos com uma vida de adoração autêntica, nos tornamos canais da Sua glória e testemunhas vivas do Seu poder transformador.",
     },
     {
         slug: "oracao-poder-intercessao",
-        titulo: "Culto de Oração: Poder da Intercessão",
-        data: "31 de maio de 2025",
-        pregador: "Pastor 2",
-        pastorLink: "/pastores#2",
-        tema: "Oração que Move o Céu",
-        imagemFundo: "/pages/mensagens/cultos/card-cultos/culto-oracao.svg",
-        resumo: "Um culto marcante onde fomos ensinados sobre o poder da intercessão na vida cristã. O Espírito Santo conduziu orações fervorosas e testemunhos impactantes de libertação foram compartilhados, ressaltando o impacto espiritual da oração persistente.",
-        referencias: [
+        title: "Culto de Oração: Poder da Intercessão",
+        date: "31 de maio de 2025",
+        preacher: "Pastor 2",
+        preacherLink: "/pastores#2",
+        theme: "Oração que Move o Céu",
+        backgroundImage: "/pages/mensagens/cultos/card-cultos/culto-oracao.svg",
+        abstract: "Um culto marcante onde fomos ensinados sobre o poder da intercessão na vida cristã. O Espírito Santo conduziu orações fervorosas e testemunhos impactantes de libertação foram compartilhados, ressaltando o impacto espiritual da oração persistente.",
+        references: [
             "Tiago 5:16 - Muito pode, por sua eficácia, a súplica do justo",
             "1 Tessalonicenses 5:17 - Orai sem cessar",
             "Filipenses 4:6 - Não andeis ansiosos... com ações de graça apresentem seus pedidos",
             "Efésios 6:18 - Orai em todo tempo com toda oração e súplica"
         ],
-        pontosPrincipais: [
+        mainPoints: [
             {
-                titulo: "O papel da fé na intercessão",
-                descricao: "Entendemos que a fé é o alicerce da oração eficaz. Aprendemos a confiar no caráter de Deus ao interceder por outros."
+                title: "O papel da fé na intercessão",
+                description: "Entendemos que a fé é o alicerce da oração eficaz. Aprendemos a confiar no caráter de Deus ao interceder por outros."
             },
             {
-                titulo: "Intercessão como ministério espiritual",
-                descricao: "Estudamos o chamado bíblico para sermos intercessores e como esse ministério é vital para a igreja."
+                title: "Intercessão como ministério espiritual",
+                description: "Estudamos o chamado bíblico para sermos intercessores e como esse ministério é vital para a igreja."
             },
             {
-                titulo: "Orando segundo a vontade de Deus",
-                descricao: "Refletimos sobre como alinhar nossas intercessões com a Palavra e os propósitos do Senhor."
+                title: "Orando segundo a vontade de Deus",
+                description: "Refletimos sobre como alinhar nossas intercessões com a Palavra e os propósitos do Senhor."
             },
             {
-                titulo: "Resultados da oração perseverante",
-                descricao: "Compartilhamos testemunhos de respostas a orações que levaram tempo, mas mostraram a fidelidade de Deus."
+                title: "Resultados da oração perseverante",
+                description: "Compartilhamos testemunhos de respostas a orações que levaram tempo, mas mostraram a fidelidade de Deus."
             }
         ],
-        conclusao: "A intercessão é uma arma poderosa nas mãos do crente. Quando oramos, Deus age. Somos convidados a entrar no lugar secreto com confiança, sabendo que nossas súplicas sobem como incenso diante do trono do Senhor."
+        conclusion: "A intercessão é uma arma poderosa nas mãos do crente. Quando oramos, Deus age. Somos convidados a entrar no lugar secreto com confiança, sabendo que nossas súplicas sobem como incenso diante do trono do Senhor."
     },
     {
         slug: "jovem-geracao-radical",
-        titulo: "Culto Jovem: Geração Radical",
-        data: "27 de maio de 2025",
-        pregador: "Pastor 2",
-        pastorLink: "/pastores#2",
-        tema: "Jovens que Fazem a Diferença",
-        imagemFundo: "/pages/mensagens/cultos/card-cultos/culto-jovem.svg",
-        resumo: "Um encontro vibrante onde os jovens foram desafiados a viver uma fé relevante. Discussões sobre identidade em Cristo, propósito e santidade marcaram a noite, junto a uma atmosfera de celebração, criatividade e comunhão.",
-        referencias: [
+        title: "Culto Jovem: Geração Radical",
+        date: "27 de maio de 2025",
+        preacher: "Pastor 2",
+        preacherLink: "/pastores#2",
+        theme: "Jovens que Fazem a Diferença",
+        backgroundImage: "/pages/mensagens/cultos/card-cultos/culto-jovem.svg",
+        abstract: "Um encontro vibrante onde os jovens foram desafiados a viver uma fé relevante. Discussões sobre identidade em Cristo, propósito e santidade marcaram a noite, junto a uma atmosfera de celebração, criatividade e comunhão.",
+        references: [
             "1 Timóteo 4:12 - Ninguém despreze a tua mocidade",
             "Romanos 12:2 - Não vos conformeis com este século",
             "Salmos 119:9 - Como purificará o jovem o seu caminho?",
             "Provérbios 3:5-6 - Confia no Senhor de todo o teu coração"
         ],
-        pontosPrincipais: [
+        mainPoints: [
             {
-                titulo: "Identidade firmada em Cristo",
-                descricao: "Abordamos como os jovens devem se reconhecer como filhos amados de Deus acima das influências sociais."
+                title: "Identidade firmada em Cristo",
+                description: "Abordamos como os jovens devem se reconhecer como filhos amados de Deus acima das influências sociais."
             },
             {
-                titulo: "Chamados à santidade",
-                descricao: "Falamos sobre a importância de viver uma vida separada para Deus, mesmo em meio a tentações do mundo moderno."
+                title: "Chamados à santidade",
+                description: "Falamos sobre a importância de viver uma vida separada para Deus, mesmo em meio a tentações do mundo moderno."
             },
             {
-                titulo: "Usando dons para impactar",
-                descricao: "Incentivamos os jovens a usarem seus talentos para edificar a igreja e transformar a sociedade."
+                title: "Usando dons para impactar",
+                description: "Incentivamos os jovens a usarem seus talentos para edificar a igreja e transformar a sociedade."
             },
             {
-                titulo: "A força da comunhão",
-                descricao: "Ressaltamos como a união entre jovens fortalece a caminhada cristã."
+                title: "A força da comunhão",
+                description: "Ressaltamos como a união entre jovens fortalece a caminhada cristã."
             }
         ],
-        conclusao: "A juventude cristã é uma geração que pode incendiar o mundo com amor, verdade e justiça. Quando os jovens se entregam verdadeiramente a Jesus, tornam-se agentes de transformação."
+        conclusion: "A juventude cristã é uma geração que pode incendiar o mundo com amor, verdade e justiça. Quando os jovens se entregam verdadeiramente a Jesus, tornam-se agentes de transformação."
     },
     {
         slug: "celebracao-honra-gloria",
-        titulo: "Culto de Celebração: Honra e Glória",
-        data: "25 de maio de 2025",
-        pregador: "Pastor 1",
-        pastorLink: "/pastores#1",
-        tema: "Glorificando a Deus com Tudo",
-        imagemFundo: "/pages/mensagens/cultos/card-cultos/culto-adoracao.svg",
-        resumo: "Culto de adoração em que a igreja expressou gratidão por bênçãos recebidas. Houve louvores, danças e ministração da Palavra destacando a importância de reconhecer publicamente os feitos do Senhor.",
-        referencias: [
+        title: "Culto de Celebração: Honra e Glória",
+        date: "25 de maio de 2025",
+        preacher: "Pastor 1",
+        preacherLink: "/pastores#1",
+        theme: "Glorificando a Deus com Tudo",
+        backgroundImage: "/pages/mensagens/cultos/card-cultos/culto-adoracao.svg",
+        abstract: "Culto de adoração em que a igreja expressou gratidão por bênçãos recebidas. Houve louvores, danças e ministração da Palavra destacando a importância de reconhecer publicamente os feitos do Senhor.",
+        references: [
             "Salmos 150 - Tudo quanto tem fôlego louve ao Senhor",
             "Salmos 103 - Bendize ó minha alma ao Senhor",
             "João 12:28 - Pai, glorifica o teu nome",
             "Apocalipse 5:13 - Ao que está assentado no trono... seja o louvor"
         ],
-        pontosPrincipais: [
+        mainPoints: [
             {
-                titulo: "O propósito da celebração",
-                descricao: "Aprendemos que celebrar ao Senhor é um ato profético que fortalece a fé."
+                title: "O propósito da celebração",
+                description: "Aprendemos que celebrar ao Senhor é um ato profético que fortalece a fé."
             },
             {
-                titulo: "Expressões de louvor",
-                descricao: "Louvor é mais que música: dançamos, pintamos e nos expressamos com liberdade."
+                title: "Expressões de louvor",
+                description: "Louvor é mais que música: dançamos, pintamos e nos expressamos com liberdade."
             },
             {
-                titulo: "O louvor como testemunho",
-                descricao: "Mostrar gratidão a Deus publicamente impacta os que ainda não crêem."
+                title: "O louvor como testemunho",
+                description: "Mostrar gratidão a Deus publicamente impacta os que ainda não crêem."
             },
             {
-                titulo: "Cultivando um coração grato",
-                descricao: "A gratidão prepara o coração para mais revelações de Deus."
+                title: "Cultivando um coração grato",
+                description: "A gratidão prepara o coração para mais revelações de Deus."
             }
         ],
-        conclusao: "Deus habita no meio dos louvores do Seu povo. Ao celebrarmos, unimos céu e terra em adoração."
+        conclusion: "Deus habita no meio dos louvores do Seu povo. Ao celebrarmos, unimos céu e terra em adoração."
     },
     {
         slug: "casais-alianca-eterna",
-        titulo: "Culto de Casais: Aliança Eterna",
-        data: "20 de maio de 2025",
-        pregador: "Pastor 1",
-        pastorLink: "/pastores#1",
-        tema: "Casamentos Fortalecidos em Cristo",
-        imagemFundo: "/pages/mensagens/cultos/card-cultos/culto-casais.svg",
-        resumo: "O culto para casais abordou temas como perdão, comunicação, respeito e unidade. Testemunhos de superação e uma renovação simbólica de votos marcaram o momento.",
-        referencias: [
+        title: "Culto de Casais: Aliança Eterna",
+        date: "20 de maio de 2025",
+        preacher: "Pastor 1",
+        preacherLink: "/pastores#1",
+        theme: "Casamentos Fortalecidos em Cristo",
+        backgroundImage: "/pages/mensagens/cultos/card-cultos/culto-casais.svg",
+        abstract: "O culto para casais abordou temas como perdão, comunicação, respeito e unidade. Testemunhos de superação e uma renovação simbólica de votos marcaram o momento.",
+        references: [
             "Efésios 5:25 - Maridos, amai vossas mulheres",
             "Gênesis 2:24 - Se tornarão uma só carne",
             "1 Coríntios 13 - O amor tudo sofre, tudo crê",
             "Cantares 8:7 - As muitas águas não podem apagar o amor"
         ],
-        pontosPrincipais: [
+        mainPoints: [
             {
-                titulo: "A base espiritual do casamento",
-                descricao: "Cristo como centro da aliança conjugal é o segredo da permanência."
+                title: "A base espiritual do casamento",
+                description: "Cristo como centro da aliança conjugal é o segredo da permanência."
             },
             {
-                titulo: "Comunicação saudável",
-                descricao: "Casais que se comunicam com empatia evitam conflitos desnecessários."
+                title: "Comunicação saudável",
+                description: "Casais que se comunicam com empatia evitam conflitos desnecessários."
             },
             {
-                titulo: "A importância do perdão",
-                descricao: "Relacionamentos duradouros são construídos sobre o perdão diário."
+                title: "A importância do perdão",
+                description: "Relacionamentos duradouros são construídos sobre o perdão diário."
             },
             {
-                titulo: "Crescendo juntos",
-                descricao: "O casamento é uma jornada de amadurecimento mútuo, não uma competição."
+                title: "Crescendo juntos",
+                description: "O casamento é uma jornada de amadurecimento mútuo, não uma competição."
             }
         ],
-        conclusao: "O matrimônio é um reflexo do amor de Cristo pela igreja. Quando marido e esposa caminham unidos em Deus, tornam-se um poderoso testemunho para o mundo."
+        conclusion: "O matrimônio é um reflexo do amor de Cristo pela igreja. Quando marido e esposa caminham unidos em Deus, tornam-se um poderoso testemunho para o mundo."
     }
 ];
